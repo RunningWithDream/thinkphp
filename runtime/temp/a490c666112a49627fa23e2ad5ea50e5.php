7efc5a64c0155ad8301401a62b624d59176d9bea
Kevin-NoteBook <kevein2011@163.com>
2018-06-13 07:09:51 +0800

    完成banner
    banner完成

--- runtime/temp/a490c666112a49627fa23e2ad5ea50e5.php
+++ runtime/temp/a490c666112a49627fa23e2ad5ea50e5.php
@@ -1,125 +1,12 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:56:"C:\xampp\htdocs\thinkphp/application/view\index\web.html";i:1528725579;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:56:"C:\xampp\htdocs\thinkphp/application/view\index\web.html";i:1528844661;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scaleable:no">
-    <title>Title</title>
-    <style>
-
-        *,
-        ::before,
-        ::after{
-            padding: 0;
-            margin: 0;
-
-            /*在移动端特殊的设置*/
-            /*清除点击高亮效果*/
-            -webkit-tap-highlight-color: transparent;
-            /*设置所有的盒子的宽度以边框开始计算*/
-            -webkit-box-sizing: border-box;
-            /*要兼容 webkit 浏览器内核厂商 这种情况一般是老的移动端浏览器*/
-            box-sizing: border-box;
-        }
-        body{
-            font-size: 14px;
-            color: #333;
-            font-family: "MicroSoft YaHei","sans-serif";/*是设备默认的字体*/
-        }
-        ul,ol{
-            list-style: none;
-        }
-        a{
-            text-decoration: none;
-            color: #333;
-        }
-        a:hover{
-            color: #333;
-        }
-        input,textarea{
-            border: none;
-            outline: none;
-            resize: none;
-            /*特殊的属性定义  清除浏览器给input自带的样式*/
-            -webkit-appearance: none;/*组件默认的样式空*/
-        }
-
-        [class^="icon_"]{
-            background: url("/public/static/image/web/sprites.png") no-repeat;
-            /*压缩背景图*/
-            background-size: 200px 200px;
-        }
-
-        .container {
-            min-width: 300px;
-            max-width: 640px;
-            margin: 0 auto;
-            position: relative;
-        }
-
-        .container .header {
-            width: 100%;
-            height: 40px;
-            position: fixed;
-            left: 0;
-            top: 0;
-            z-index: 1;
-            background-color: #2dc49b;
-        }
-
-        .container .header .header-box {
-            max-width: 640px;
-            min-width: 300px;
-            margin: 0 auto;
-            height: 40px;
-            position: relative;
-        }
-
-        .container .header .header-box .icon_logo {
-            background-position: 0 -103px;
-            position: absolute;
-            left: 10px;
-            top: 4px;
-            height: 36px;
-            width: 60px;
-        }
-
-        .container .header .header-box form {
-            width: 100%;
-            padding-left: 75px;
-            padding-right: 55px;
-            position: relative;
-        }
-
-        .container .header .header-box .icon_search {
-            height: 20px;
-            width: 20px;
-            background-position: -60px -109px;
-            position: absolute;
-            left: 80px;
-            top: 10px;
-        }
-
-        .container .header .header-box .search {
-            width: 100%;
-            border-radius: 15px;
-            height: 30px;
-            margin-top: 5px;
-            padding-left: 30px;
-        }
-
-        .container .header .header-box .search-btn {
-            position: absolute;
-            right: 0;
-            top: 0;
-            width: 50px;
-            height: 40px;
-            line-height: 40px;
-            text-align: center;
-            font-size: 15px;
-            color: #fff;
-        }
-    </style>
+    <title>移动商城</title>
+    <link charset="utf-8" rel="stylesheet" href="/public/static/css/index/base.css">
+    <link charset="utf-8" rel="stylesheet" href="/public/static/css/index/web.css">
 </head>
 <body>
 
@@ -134,6 +21,34 @@
             <a href="#" class="search-btn">搜索</a>
         </div>
     </div>
+
+
+    <div class="banner">
+        <ul class="clearfix">
+            <li><a href=""><img src="/public/static/image/web//l8.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l1.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l2.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l3.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l4.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l5.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l6.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l7.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l8.jpg" alt=""></a></li>
+            <li><a href=""><img src="/public/static/image/web//l1.jpg" alt=""></a></li>
+        </ul>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+
+
 </div>
 <script>
 

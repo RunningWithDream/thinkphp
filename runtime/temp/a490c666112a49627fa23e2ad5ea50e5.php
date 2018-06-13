fcac4e63af3b8ec33f99827613bc64f1e19dd0c9
Kevin-NoteBook <kevein2011@163.com>
2018-06-13 18:00:53 +0800

    完成轮播图特效
    1.自动滚动
    2.无缝滚动
    3.触摸滑动
    4.滑动一定距离吸附
    5.超过一定距离滚动

--- runtime/temp/a490c666112a49627fa23e2ad5ea50e5.php
+++ runtime/temp/a490c666112a49627fa23e2ad5ea50e5.php
@@ -1,9 +1,9 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:56:"C:\xampp\htdocs\thinkphp/application/view\index\web.html";i:1528856845;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:56:"C:\xampp\htdocs\thinkphp/application/view\index\web.html";i:1528881613;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scaleable:no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=0">
     <title>移动商城</title>
     <link charset="utf-8" rel="stylesheet" href="/public/static/css/index/base.css">
     <link charset="utf-8" rel="stylesheet" href="/public/static/css/index/web.css">
@@ -35,7 +35,7 @@
             <li><a href=""><img src="/public/static/image/web/l1.jpg" alt=""></a></li>
         </ul>
         <ul>
-            <li></li>
+            <li class="now"></li>
             <li></li>
             <li></li>
             <li></li>
@@ -177,47 +177,8 @@
     </div>
 
 </div>
-<script>
 
-    //     事件类型：
-    //
-    // touchstart : 触摸开始（手指放在触摸屏上）
-    //
-    // touchmove : 拖动（手指在触摸屏上移动）
-    //
-    // touchend : 触摸结束（手指从触摸屏上移开）
-    //
-    // touchenter ：移动的手指进入一个dom元素。
-    //
-    // touchleave ：移动的手指离开一个dom元素。
-    //
-    // 还有一个touchcancel，是在拖动中断时候触发。
-    var div = document.querySelector('div');
-    div.addEventListener('touchstart', function (e) {
-        // e.preventDefault();//阻止事件继续传递
-        console.log(e);
-        console.log(e.targetTouches);//当前目标元素所有触摸点，支持多点触摸，是一个数组
-        console.log(e.changedTouches);//最近改变的触摸点，包括屏幕外不显示的触摸点
-        console.log(e.touches);//当前屏幕所有触摸点，不包括屏幕外的
-
-        console.log(e.targetTouches[0].clientX);//
-        console.log(e.targetTouches[0].clientX);//
-        console.log(e.targetTouches[0].clientX);//
-
-        console.log("touch start");
-    });
-
-    div.addEventListener('touchmove', function (e) {
-        console.log("touch move");
-    });
-
-    div.addEventListener('touchend', function (e) {
-        console.log("touch end");
-    });
-
-    div.addEventListener('click', function (e) {
-        console.log("click");
-    });
-</script>
+<script src="/public/static/js/index/common.js"></script>
+<script src="/public/static/js/index/web.js"></script>
 </body>
 </html>
